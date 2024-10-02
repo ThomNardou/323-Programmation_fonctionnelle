3b1bf71c13020e9e6654cfd457a39084564347c7
Tytan <thomasnardou@icloud.com>
2024-10-02 11:27:10 +0200

    add (silkway) add the algorithm in order to detect if if the king can go to H8

--- personnel/silkroad/ConsoleApp1/Program.cs
+++ personnel/silkroad/ConsoleApp1/Program.cs
@@ -3,6 +3,26 @@
 silkyWay[0, 0] = true; // A1
 silkyWay[7, 7] = true; // H8
 
+// Place de la soie aléatoirement
+Random randomColumn = new Random();
+Random randomRow = new Random();
+
+for (int i = 0; i<silkyWay.Length/2-2; i++)
+{
+    // Logique pour placer de la soie à un endroit aléatoirement
+    int column = randomColumn.Next(8);
+    int row = randomRow.Next(8);
+
+    while (silkyWay[column, row]
+        )
+    {
+        column = randomColumn.Next(8);
+        row = randomRow.Next(8);
+    }
+
+    silkyWay[column, row] = true;
+}
+
 void DrawBoard(bool[,] board)
 {
     Console.WriteLine("  12345678");
@@ -31,6 +51,12 @@ void DrawBoard(bool[,] board)
 DrawBoard(silkyWay);
 
 // TODO Create a data structure that allow us to remember which square has already been tested
+bool[,] testedSquares = new bool[8, 8];
+
+bool SquareHasBeenTested(int column, int row, bool[,] array)
+{
+    return array[column, row];
+}
 
 // TODO Create a data structure that allow us to remember the successful steps
 
@@ -45,6 +71,29 @@ DrawBoard(silkyWay);
 //
 //          2. Je peux sortir depuis une des cases où je peux aller (et où je ne suis pas encore allé)
 
+bool HaveAnyWay(int x, int y)
+{
+    if (x == 7 && y == 7) return true;
+    
+    if (x < 0 || x > 7 || y < 0 || y > 7 || !silkyWay[x,y] || SquareHasBeenTested(x,y,testedSquares) ) return false;
+    testedSquares[x, y] = true;
+
+    if (
+        HaveAnyWay(x+1,y) || 
+        HaveAnyWay(x-1,y) || 
+        HaveAnyWay(x,y+1) || 
+        HaveAnyWay(x,y-1) || 
+        HaveAnyWay(x+1,y+1) || 
+        HaveAnyWay(x-1,y-1) ||
+        HaveAnyWay(x+1,y-1) ||
+        HaveAnyWay(x-1,y+1)
+     ) return true;
+
+    return false;
+}
+
 // TODO Call the function and show the results
+Console.WriteLine("\n\n**********************************************************************************");
+Console.WriteLine(HaveAnyWay(0,0));
 
 Console.ReadLine();
